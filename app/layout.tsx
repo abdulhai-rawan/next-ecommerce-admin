434f070f09b149dd4f2d1d8d8e0df9c256163422
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-04-26 10:44:36 +0430

    Dark Mode (Admin) Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import { ToastProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,9 +23,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ModalProvider />
-          {children}
-          <ToastProvider />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider />
+            {children}
+            <ToastProvider />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import { UserButton, auth } from "@clerk/nextjs";
 
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
+import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
 import { redirect } from "next/navigation";
 
@@ -18,6 +19,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton signInUrl="/" />
         </div>
       </div>
